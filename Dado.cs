d29bd4901c62ba1e07f65f20a79be38a94b70d51
MarouanBouleh <marouan.bouleh.studenti@isii.it>
2025-11-21 00:54:18 +0100

    Add files via upload
    k

---
+++ Dado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public  class Dado
+    {
+        public int nl;
+        private Random rnd;
+        
+
+        public Dado():this(6)
+        { }
+        public Dado(int nf)
+        {
+            nl= nf;
+            rnd = new Random();
+        }
+        public int Lancia()
+        {
+            return rnd.Next(1,nl+1);
+        }
+    }
+    
+
+}
