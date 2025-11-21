15e0e9d5aaa17e9e6eecb57a6791eca97bb0ce79
MarouanBouleh <marouan.bouleh.studenti@isii.it>
2025-11-21 01:04:03 +0100

    Add files via upload

---
+++ Pacco.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{    //CALCOLO VOLUME 
+    public class Pacco
+     {
+        public double bas;
+        public double alt;
+        public double prof;
+        public int Num;
+        //costruttore 
+        public Pacco(double b,double a,double p,int n) 
+        { 
+          bas= b; alt= a; prof= p; Num = n;
+        }
+        public override string ToString()
+        {
+            return Num+"\tb:"+bas+"\th:"+alt+"\tp:"+prof;
+        }
+        public double Volume()
+        {
+            return bas*alt*prof;
+        }
+    }
+}
